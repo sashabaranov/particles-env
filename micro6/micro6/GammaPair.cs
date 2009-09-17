4a0200e946e8eb214d6f86bfdb8c6f2ba4e1a9dd
scratalex <scratalex@gmail.com>
2009-09-17 12:12:21 +0000

    улаживаем проблемы с micro6 - заливаем нормальную версию

---
+++ micro6/micro6/GammaPair.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace micro6
+{
+    class GammaPair
+    {
+        GammaParticle Minor;
+        GammaParticle Major;
+
+        public int ParticlesCount;
+        public static int bottom;
+        public static int middle;
+
+        public GammaPair(double energy)
+        {
+            Minor = new GammaParticle();
+            Minor.CountEnergy(energy, true);
+
+            Major = new GammaParticle();
+            Major.CountEnergy(energy, false);
+        }
+        
+        public static bool operator ==(GammaPair First, GammaPair Second)
+        {
+            if (First.Major.Energy == Second.Major.Energy &&
+                First.Minor.Energy == Second.Minor.Energy) return true;
+            else return false;
+        }
+
+        public static bool operator !=(GammaPair First, GammaPair Second)
+        {
+            if (First.Major.Energy != Second.Major.Energy &&
+                First.Minor.Energy != Second.Minor.Energy) return true;
+            else return false;
+        }
+
+        public Point[] PointsToDraw()
+        {
+
+            Point[] Points = { 
+                                new Point((int)(Minor.Energy+middle), bottom),
+                                new Point((int)(Minor.Energy+middle), bottom - ParticlesCount),
+                                new Point((int)(Major.Energy+middle), bottom - ParticlesCount),
+                                new Point((int)(Major.Energy+middle), bottom)
+                             };
+            return Points;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}:{2}\n", Minor.ToString(), Major.ToString(), ParticlesCount);
+        }
+    }
+}
